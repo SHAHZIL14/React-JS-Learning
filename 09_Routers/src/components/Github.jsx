42bb6ba099faa964214f6a2013882b506e2104cf
Mohd Shazil Raza <shazilraza8@gmail.com>
2025-07-10 16:49:55 +0530

    ContextAPI1

--- 09_Routers/src/components/Github.jsx
+++ 09_Routers/src/components/Github.jsx
@@ -1,37 +1,23 @@
-import { useParams } from "react-router";
-import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 function Github() {
-  const { userName } = useParams();
-  let [isLoading, setIsLoading] = useState(true);
-  let [data, setData] = useState({});
-  useEffect(() => {
-    const url = `https://api.github.com/users/${userName}`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((response) => {
-        setData(response);
-        setIsLoading(false);
-      });
-  }, []);
-
-  if (isLoading) {
-    return (
-      <div
-        className="h-48 w-48 m-auto rounded-[50%] border-2 border-transparent border-l-black animate-spin
-        "
-      ></div>
-    );
-  }
+  const data = useLoaderData();
+  console.log(data);
   return (
     <>
-      {}
-
-      <div className="container h-full w-1/3 m-auto flex flex-col justify-center items-center  ">
-        <img src={data.avatar_url} alt="" />
-        <h1>{data.name}</h1>
+      <div className="container h-full w-1/3 m-auto flex bg-amber-500 flex-col justify-center items-center  ">
+        <img src={data?.avatar_url} alt="" />
+        <h1>{data?.name}</h1>
       </div>
     </>
   );
 }
-
 export default Github;
+
+export function githubLoader() {
+  const url = `https://api.github.com/users/shahzil14}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((response) => {
+      return response;
+  });
+}
--- 09_Routers/src/main.jsx
+++ 09_Routers/src/main.jsx
@@ -7,33 +7,19 @@ import {
 } from "react-router-dom";
 import "./index.css";
 import { Layout, Home, About, Userpage, Github, Contact } from "./index";
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Layout />,
-//     children: [
-//       {
-//         path: "",
-//         element: <Home />,
-//       },
-//       {
-//         path: "about",
-//         element: <About />,
-//       },
-//       {
-//         path: "contact",
-//         element: <Contact />,
-//       },
-//     ],
-//   },
-// ]);
+import { githubLoader } from "./components/Github";
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="" element={<Home />} />
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />} />
-      <Route path="user/:userName" element={<Github/>} />
+      <Route
+        loader={githubLoader}
+        path="github"
+        element={<Github />}
+      />
     </Route>
   )
 );
